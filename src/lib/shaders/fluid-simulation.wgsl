bac11867358ab466635fa5887ace94d7f893efb8
Dale Xu <dalexu1@outlook.com>
2024-04-12 19:09:11 -0400

    Perform fluid simulation at lower resolution than density buffer

--- src/lib/shaders/fluid-simulation.wgsl
+++ src/lib/shaders/fluid-simulation.wgsl
@@ -1,24 +1,26 @@
 const DT: f32 = 1;
+const SCALE: f32 = 3;
 
-const DIFFUSION: f32 = 0.99;
-const CURL: f32 = 0.2;
+const VELOCITY_DIFFUSION: f32 = 0.998;
+const DENSITY_DIFFUSION: f32 = 0.99;
 
-const RADIUS: f32 = 10;
+const CURL: f32 = 0.12;
 
 @group(0) @binding(0) var texture: texture_storage_2d<rgba8unorm, write>;
 @group(0) @binding(1) var<uniform> dimensions: vec2u;
+@group(0) @binding(2) var<uniform> densityDimensions: vec2u;
 
-@group(0) @binding(2) var<storage, read_write> velocity: array<vec2f>;
-@group(0) @binding(3) var<storage> previousVelocity: array<vec2f>;
+@group(1) @binding(0) var<storage, read_write> velocity: array<vec2f>;
+@group(1) @binding(1) var<storage> previousVelocity: array<vec2f>;
 
-@group(0) @binding(4) var<storage, read_write> divergence: array<f32>;
-@group(0) @binding(5) var<storage, read_write> curl: array<f32>;
+@group(1) @binding(2) var<storage, read_write> divergence: array<f32>;
+@group(1) @binding(3) var<storage, read_write> curl: array<f32>;
 
-@group(0) @binding(6) var<storage, read_write> pressure: array<f32>;
-@group(0) @binding(7) var<storage> previousPressure: array<f32>;
+@group(1) @binding(4) var<storage, read_write> pressure: array<f32>;
+@group(1) @binding(5) var<storage> previousPressure: array<f32>;
 
-@group(0) @binding(8) var<storage, read_write> density: array<vec3f>;
-@group(0) @binding(9) var<storage> previousDensity: array<vec3f>;
+@group(1) @binding(6) var<storage, read_write> density: array<vec3f>;
+@group(1) @binding(7) var<storage> previousDensity: array<vec3f>;
 
 // Helper function to index buffer using two dimensional index
 fn indexu(id: vec2u) -> u32 { return id.y * dimensions.x + id.x; }
@@ -39,16 +41,6 @@ fn bilinear2f(data: ptr<storage, array<vec2f>>, id: vec2f) -> vec2f
         mix(data[index(ij.zy)], data[index(ij.zw)], t.y), t.x);
 }
 
-fn bilinear3f(data: ptr<storage, array<vec3f>>, id: vec2f) -> vec3f
-{
-    let i = vec2i(floor(id)); let ij = vec4i(i, i + 1);
-    let t = id - vec2f(i);
-
-    return mix(
-        mix(data[index(ij.xy)], data[index(ij.xw)], t.y),
-        mix(data[index(ij.zy)], data[index(ij.zw)], t.y), t.x);
-}
-
 @compute @workgroup_size(8, 8)
 fn advection(@builtin(global_invocation_id) id: vec3u)
 {
@@ -59,7 +51,7 @@ fn advection(@builtin(global_invocation_id) id: vec3u)
     let previous = vec2f(id.xy) - previousVelocity[i] * DT;
 
     // Interpolation of four pixels surrounding previous position
-    velocity[i] = DIFFUSION * bilinear2f(&previousVelocity, previous);
+    velocity[i] = VELOCITY_DIFFUSION * bilinear2f(&previousVelocity, previous);
 }
 
 @compute @workgroup_size(8, 8)
@@ -141,38 +133,66 @@ fn gradientSubtraction(@builtin(global_invocation_id) id: vec3u)
     velocity[indexu(id.xy)] -= 0.5 * vec2f(r - l, u - d) * DT;
 }
 
+// Helper function to index buffer using two dimensional index
+fn sindexu(id: vec2u) -> u32 { return id.y * densityDimensions.x + id.x; }
+fn sindex(id: vec2i) -> u32
+{
+    let d = vec2i(densityDimensions);
+    return sindexu(vec2u((id + d) % d));
+}
+
+fn bilinear3f(data: ptr<storage, array<vec3f>>, id: vec2f) -> vec3f
+{
+    let i = vec2i(floor(id)); let ij = vec4i(i, i + 1);
+    let t = id - vec2f(i);
+
+    return mix(
+        mix(data[sindex(ij.xy)], data[sindex(ij.xw)], t.y),
+        mix(data[sindex(ij.zy)], data[sindex(ij.zw)], t.y), t.x);
+}
+
 @compute @workgroup_size(8, 8)
 fn densityAdvection(@builtin(global_invocation_id) id: vec3u)
 {
-    if (id.x >= dimensions.x || id.y >= dimensions.y) { return; }
+    if (id.x >= densityDimensions.x || id.y >= densityDimensions.y) { return; }
 
     // Move density based on velocity field
-    let i = indexu(id.xy);
-    let previous = vec2f(id.xy) - previousVelocity[i] * DT;
+    let i = vec2f(id.xy);
+    let previous = vec2f(id.xy) - SCALE * bilinear2f(&previousVelocity, i / SCALE) * DT;
 
-    density[i] = DIFFUSION * bilinear3f(&previousDensity, previous);
+    density[sindexu(id.xy)] = DENSITY_DIFFUSION * bilinear3f(&previousDensity, previous);
 }
 
 @compute @workgroup_size(8, 8)
 fn transfer(@builtin(global_invocation_id) id: vec3u)
 {
     // Transfer density data as colors to texture
-    let color = vec4f(density[indexu(id.xy)], 1);
+    let color = vec4f(density[sindexu(id.xy)], 1);
     textureStore(texture, id.xy, color);
 }
 
 // User input passes
-@group(1) @binding(0) var<uniform> position: vec2f;
-@group(1) @binding(1) var<uniform> impulse: vec2f;
-@group(1) @binding(2) var<uniform> color: vec3f;
+@group(2) @binding(0) var<uniform> position: vec2f;
+@group(2) @binding(1) var<uniform> impulse: vec2f;
+@group(2) @binding(2) var<uniform> color: vec3f;
+@group(2) @binding(3) var<uniform> radius: f32;
+
+@compute @workgroup_size(8, 8)
+fn addImpulse(@builtin(global_invocation_id) id: vec3u)
+{
+    let offset = vec2f(id.xy) - vec2f(radius);
+    if (dot(offset, offset) > radius * radius)  { return; }
+
+    let i = index(vec2i(position + offset));
+    velocity[i] += impulse;
+}
 
 @compute @workgroup_size(8, 8)
 fn addDensity(@builtin(global_invocation_id) id: vec3u)
 {
-    let offset = vec2f(id.xy) - vec2f(RADIUS);
-    if (dot(offset, offset) > RADIUS * RADIUS)  { return; }
+    let offset = vec2f(id.xy) - vec2f(radius);
+    if (dot(offset, offset) > radius * radius)  { return; }
 
-    let i = index(vec2i(position + offset));
-    velocity[i] += impulse;
+    let i = sindex(vec2i(SCALE * position + offset));
     density[i] += color;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@ import { onMount } from "svelte"
 
 import Device, { ComputePass, ComputePipeline, LoadOperation, RenderPass, RenderPipeline, VertexFormat } from "../lib/Device"
 import { Color3, Color4, Vector2 } from "../lib/Math"
-import { Buffer, BufferFormat, Sampler, Shader, Texture, TextureFormat } from "../lib/Resource"
+import { Buffer, BufferFormat, Sampler, SamplerFilterMode, Shader, Texture, TextureFormat } from "../lib/Resource"
 import Input, { MouseButton } from "../lib/Input"
 
 import textureCode from "../lib/shaders/texture.wgsl?raw"
@@ -23,17 +23,20 @@ async function main()
 {
     let device = await Device.init(canvas)
 
+    const SCALE = 3
     const ITERATIONS = 32
-
-    const HEIGHT = 300
-    const WIDTH = Math.floor(HEIGHT * canvas.width / canvas.height)
 
     const RADIUS = 10
 
+    let width = window.innerWidth, height = window.innerHeight
+    let w = Math.floor(width / SCALE), h = Math.floor(height / SCALE)
+
     // Initialize rendering texture related data
-    let texture = new Texture(device, TextureFormat.RGBA_UNORM, GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_DST, [WIDTH, HEIGHT])
+    let texture = new Texture(device, TextureFormat.RGBA_UNORM, GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_DST, [width, height])
     let dimensionsBuffer = new Buffer(device, BufferFormat.U32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 2)
-    dimensionsBuffer.write([WIDTH, HEIGHT])
+    let densityDimensionsBuffer = new Buffer(device, BufferFormat.U32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 2)
+    dimensionsBuffer.write([w, h])
+    densityDimensionsBuffer.write([width, height])
 
     let vertexBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST, 2 * 6)
     vertexBuffer.write(
@@ -43,51 +46,61 @@ async function main()
     ])
 
     let pipeline = new RenderPipeline(device, new Shader(device, textureCode), device.format, [{ format: VertexFormat.F32_2 }])
-    let pass = new RenderPass(pipeline, [[{ i: 0, resource: new Sampler(device) }, { i: 1, resource: texture }]], [vertexBuffer])
+    let pass = new RenderPass(pipeline, [[{ i: 0, resource: new Sampler(device, { mag: SamplerFilterMode.LINEAR }) }, { i: 1, resource: texture }]], [vertexBuffer])
 
     // Initialize fluid simulation buffers
-    let velocityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, WIDTH * HEIGHT * 2)
-    let previousVelocityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, WIDTH * HEIGHT * 2)
+    let velocityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, w * h * 2)
+    let previousVelocityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, w * h * 2)
 
-    let divergenceBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE, WIDTH * HEIGHT)
-    let curlBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE, WIDTH * HEIGHT)
+    let divergenceBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE, w * h)
+    let curlBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE, w * h)
 
-    let pressureBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, WIDTH * HEIGHT)
-    let previousPressureBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, WIDTH * HEIGHT)
+    let pressureBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, w * h)
+    let previousPressureBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, w * h)
 
-    let densityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, WIDTH * HEIGHT * 4)
-    let previousDensityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, WIDTH * HEIGHT * 4)
+    let densityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, width * height * 4)
+    let previousDensityBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, width * height * 4)
 
+    // Initialize shader passes
     let shader = new Shader(device, fluidSimulationCode)
 
     let advectionPass = new ComputePass(new ComputePipeline(device, shader, "advection"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 3, resource: previousVelocityBuffer }]])
-    let calculateDivergencePass = new ComputePass(new ComputePipeline(device, shader, "calculateDivergence"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 4, resource: divergenceBuffer }]])
-    let calculateCurlPass = new ComputePass(new ComputePipeline(device, shader, "calculateCurl"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 5, resource: curlBuffer }]])
-    let vorticityPass = new ComputePass(new ComputePipeline(device, shader, "vorticity"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 5, resource: curlBuffer }]])
-    let jacobiPass = new ComputePass(new ComputePipeline(device, shader, "jacobi"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 4, resource: divergenceBuffer }, { i: 6, resource: pressureBuffer }, { i: 7, resource: previousPressureBuffer }]])
-    let gradientSubtractionPass = new ComputePass(new ComputePipeline(device, shader, "gradientSubtraction"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 6, resource: pressureBuffer }]])
-    let densityAdvectionPass = new ComputePass(new ComputePipeline(device, shader, "densityAdvection"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 3, resource: previousVelocityBuffer }, { i: 8, resource: densityBuffer }, { i: 9, resource: previousDensityBuffer }]])
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }, { i: 1, resource: previousVelocityBuffer }]])
 
+    let calculateDivergencePass = new ComputePass(new ComputePipeline(device, shader, "calculateDivergence"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }, { i: 2, resource: divergenceBuffer }]])
+    let calculateCurlPass = new ComputePass(new ComputePipeline(device, shader, "calculateCurl"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }, { i: 3, resource: curlBuffer }]])
+    let vorticityPass = new ComputePass(new ComputePipeline(device, shader, "vorticity"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }, { i: 3, resource: curlBuffer }]])
+
+    let jacobiPass = new ComputePass(new ComputePipeline(device, shader, "jacobi"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 2, resource: divergenceBuffer }, { i: 4, resource: pressureBuffer }, { i: 5, resource: previousPressureBuffer }]])
+    let gradientSubtractionPass = new ComputePass(new ComputePipeline(device, shader, "gradientSubtraction"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }, { i: 4, resource: pressureBuffer }]])
+
+    let densityAdvectionPass = new ComputePass(new ComputePipeline(device, shader, "densityAdvection"),
+        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: densityDimensionsBuffer }], [{ i: 1, resource: previousVelocityBuffer }, { i: 6, resource: densityBuffer }, { i: 7, resource: previousDensityBuffer }]])
     let transferPass = new ComputePass(new ComputePipeline(device, shader, "transfer"),
-        [[{ i: 0, resource: texture }, { i: 1, resource: dimensionsBuffer }, { i: 8, resource: densityBuffer }]])
+        [[{ i: 0, resource: texture }, { i: 2, resource: densityDimensionsBuffer }], [{ i: 6, resource: densityBuffer }]])
 
     // Initialize user interface uniforms
     let positionBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 2)
     let impulseBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 2)
     let colorBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 3)
+    let radiusBuffer = new Buffer(device, BufferFormat.F32, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST, 1)
 
+    let addImpulsePass = new ComputePass(new ComputePipeline(device, shader, "addImpulse"),
+        [[{ i: 1, resource: dimensionsBuffer }], [{ i: 0, resource: velocityBuffer }], [{ i: 0, resource: positionBuffer }, { i: 1, resource: impulseBuffer }, { i: 3, resource: radiusBuffer }]])
     let addDensityPass = new ComputePass(new ComputePipeline(device, shader, "addDensity"),
-        [[{ i: 1, resource: dimensionsBuffer }, { i: 2, resource: velocityBuffer }, { i: 8, resource: densityBuffer }], [{ i: 0, resource: positionBuffer }, { i: 1, resource: impulseBuffer }, { i: 2, resource: colorBuffer }]])
+        [[{ i: 2, resource: densityDimensionsBuffer }], [{ i: 6, resource: densityBuffer }], [{ i: 0, resource: positionBuffer }, { i: 2, resource: colorBuffer }, { i: 3, resource: radiusBuffer }]])
 
-    let w = Math.ceil(WIDTH / 8), h = Math.ceil(HEIGHT / 8)
-    let r = Math.ceil((2 * RADIUS + 1) / 8)
+
+    let x = Math.ceil(w / 8), y = Math.ceil(h / 8)
+    let nx = Math.ceil(width / 8), ny = Math.ceil(height / 8)
+
+    let r = Math.ceil((2 * RADIUS - 1) / 8)
+    let nr = Math.ceil((2 * SCALE * RADIUS + 1) / 8)
 
     let previous = Input.mouse
 
@@ -97,36 +110,39 @@ async function main()
         let position = Input.mouse
         if (Input.button[MouseButton.LEFT])
         {
-            let localPosition = new Vector2(position.x * WIDTH / canvas.width, HEIGHT - position.y * HEIGHT / canvas.height)
-            let localPrevious = new Vector2(previous.x * WIDTH / canvas.width, HEIGHT - previous.y * HEIGHT / canvas.height)
+            let localPosition = new Vector2(position.x * w / canvas.width, h - position.y * h / canvas.height)
+            let localPrevious = new Vector2(previous.x * w / canvas.width, h - previous.y * h / canvas.height)
             let impulse = localPosition.sub(localPrevious)
 
             positionBuffer.write([localPosition])
             impulseBuffer.write([impulse])
-            colorBuffer.write([Color3.WHITE.mul(0.15 * Math.sqrt(impulse.length))])
+            radiusBuffer.write([RADIUS - 1])
+            addImpulsePass.dispatch(r, r)
 
-            addDensityPass.dispatch(r, r)
+            device.submit()
+            colorBuffer.write([Color3.WHITE.mul(0.1 * Math.sqrt(impulse.length))])
+            radiusBuffer.write([SCALE * RADIUS])
+            addDensityPass.dispatch(nr, nr)
         }
         previous = position
 
         device.copyBuffer(velocityBuffer, previousVelocityBuffer)
         device.copyBuffer(densityBuffer, previousDensityBuffer)
 
-        advectionPass.dispatch(w, h)
-        calculateDivergencePass.dispatch(w, h)
-        calculateCurlPass.dispatch(w, h)
-        vorticityPass.dispatch(w, h)
+        advectionPass.dispatch(x, y)
+        calculateDivergencePass.dispatch(x, y)
+        calculateCurlPass.dispatch(x, y)
+        vorticityPass.dispatch(x, y)
 
         for (let i = 0; i < ITERATIONS; i++)
         {
             device.copyBuffer(pressureBuffer, previousPressureBuffer)
-            jacobiPass.dispatch(w, h)
+            jacobiPass.dispatch(x, y)
         }
+        gradientSubtractionPass.dispatch(x, y)
 
-        gradientSubtractionPass.dispatch(w, h)
-        densityAdvectionPass.dispatch(w, h)
-
-        transferPass.dispatch(w, h)
+        densityAdvectionPass.dispatch(nx, ny)
+        transferPass.dispatch(nx, ny)
 
         device.beginPass(device.texture, { load: LoadOperation.CLEAR, color: Color4.BLACK })
         pass.render(6)
